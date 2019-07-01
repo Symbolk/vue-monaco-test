99b57b989f6727827936b4536ab89a25ae667919
Symbolk <symbolk@163.com>
2019-07-01 17:52:05 +0800

    Restore the webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     new MonacoWebpackPlugin({
       // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-      languages: ['json']
+      languages: ['javascript']
     })
   ]
 };
